61627c600ade3c07c406f3bbdf0fb89f5812ada7
MichelBARRAT <michel.barrat.42@gmail.com>
2019-12-03 09:59:02 +0100

    bo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import AgendaComponent from './component/AgendaComponent';
+import AgendaComponent from './component/AgendaComponent.js';
 import './App.css';
 
 class App extends React.Component {
--- src/component/AgendaComponent.js
+++ src/component/AgendaComponent.js
@@ -1,13 +1,39 @@
 import React from 'react';
-
+import { L10n } from '@syncfusion/ej2-base';
 import { Inject, ScheduleComponent, Day, Week, WorkWeek, Month, Agenda } from '@syncfusion/ej2-react-schedule';
+import { DataManager, ODataV4Adaptor, Query } from '@syncfusion/ej2-data';
 
-class AgendaComponent extends React.Component {
+L10n.load({
+    "en-US": {
+        "schedule": {
+            "title": "Professeur",
+            "location": "Cour"
+
+        },
+        "recurrenceeditor": {
+            "none": "None"
+        }
+    }
+});
+
+class AgendaComponent extends React.Component, ODataV4Adaptor {
+    constructor() {
+        super(...arguments);
+        this.dataManager = new DataManager({
+            url: 'https://js.syncfusion.com/demos/ejservices/api/Schedule/LoadData',
+            adaptor: new ODataV4Adaptor
+        });
+        this.dataQuery = new Query().from("Events");
+
+
+    }
+
     render() {
-        return <ScheduleComponent>
+        return <ScheduleComponent startHour={'08:00'} endHour={'18:00'} firstDayOfWeek={1}>
             <Inject services={[Day, Week, WorkWeek, Month, Agenda]} />
         </ScheduleComponent>
-    }
+    };
+
 }
 
 export default AgendaComponent;
